2818d85e30ee27a4dc56ad6792e85009c7a7b232
Tarunakanta kumbhar <btkkumbhar1998@gmail.com>
2024-07-09 12:08:27 +0530

    done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       {/* LEFT */}
       <div className="md:hidden lg:block w-[20%]">
         <Link href="/" className="font-bold text-xl text-blue-600">
-          LAMASOCIAL
+          LinkUp
         </Link>
       </div>
       {/* CENTER */}
@@ -53,9 +53,13 @@ const Navbar = () => {
             <span>Stories</span>
           </Link>
         </div>
-        <div className='hidden xl:flex p-2 bg-slate-100 items-center rounded-xl'>
-          <input type="text" placeholder="search..." className="bg-transparent outline-none"/>
-          <Image src="/search.png" alt="" width={14} height={14}/>
+        <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+          <input
+            type="text"
+            placeholder="search..."
+            className="bg-transparent outline-none"
+          />
+          <Image src="/search.png" alt="" width={14} height={14} />
         </div>
       </div>
       {/* RIGHT */}
